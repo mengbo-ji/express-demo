298f27ab9af2d66c5db1f9c983f9a9c70640ec54
mengbo-ji <jmb33595@163.com>
2022-01-18 20:17:12 +0800

    feat: 第三方中间件

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import * as express from 'express'
+import * as path from 'path'
+import * as morgan from 'morgan'
 
 const app = express()
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.get('/', (req, res) => {
   res.send('hello world')
