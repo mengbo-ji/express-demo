56b5faaff8a1d3feeb1c3cecf53802c8b12b8919
mengbo-ji <jmb33595@163.com>
2022-01-18 22:54:07 +0800

    feat: exporess-demo

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,36 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+app.get('/demo.test', (req, res) => {
+  res.send('demo.test')
+})
+
+// app.get('/ab?cd', (req, res) => {
+//   res.send(req.url)
+// })
+
+// app.get('/ab*cd', (req, res) => {
+//   res.send(req.url)
+// })
+
+// app.get('/ab(cd)?e', (req, res) => {
+//   res.send(req.url)
+// })
+
+// app.get(/a/, (req, res) => {
+//   res.send(req.url)
+// })
+
+// app.get(/.*fly$/, (req, res) => {
+//   res.send(req.url)
+// })
+
+// app.get('/user/:id', (req, res) => {
+//   res.send(req.url)
+// })
+
+app.get('/user/:id(\\d+)', (req, res) => {
+  res.send(req.url)
+})
+
 app.listen(1234)
